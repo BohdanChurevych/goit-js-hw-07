c68988a826f1276940ed3668908f9307096024d5
BohdanChurevych <bohdan.churevych@gmail.com>
2023-02-27 17:00:18 +0200

    1

---
+++ js/01-gallery.js
@@ -0,0 +1,23 @@
+import { galleryItems } from "./gallery-items.js";
+
+const createGalleryCardsMarkup = (galleryItems) => {
+  const { preview, original, description } = galleryItems;
+  return `<div class="gallery__item"><a class="gallery__link" href="large-image.jpg"><img class="gallery__image" src=${preview} data-source=${original} alt=${description} /></a></div>`;
+};
+
+const gallery = document.querySelector(".gallery");
+const createGallery = galleryItems.map(createGalleryCardsMarkup).join("");
+gallery.insertAdjacentHTML("beforeend", createGallery);
+
+gallery.addEventListener("click", selectModalImg);
+function selectModalImg(e) {
+  e.preventDefault();
+  if (e.target.nodeName !== "IMG") {
+    return;
+  }
+  const selectedImg = e.target.dataset.source;
+  const instance = basicLightbox.create(`
+    <img src=${selectedImg} alt="image" />
+    `);
+  instance.show();
+}
---
+++ js/02-lightbox.js
@@ -0,0 +1,14 @@
+import { galleryItems } from './gallery-items.js';
+
+const createGalleryMarkup = (galleryItems) => {
+    const { preview, original, description } = galleryItems;
+    return `<a class="gallery__item" href="${original}"><img class="gallery__image" src=${preview} alt=${description} title=${description} /></a>`;
+};
+  
+const gallery = document.querySelector(".gallery");
+const createGallery = galleryItems.map(createGalleryMarkup).join("");
+gallery.insertAdjacentHTML("beforeend", createGallery);
+  
+gallery.insertAdjacentHTML("afterbegin", createGallery);
+
+const lightbox = new SimpleLightbox('.gallery a', {captionDelay:250});
